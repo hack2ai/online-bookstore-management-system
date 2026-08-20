b4feaafa75d5749d6f233990f5d32bc34801a5cf
Pankaj Kumar <83492104+hack2ai@users.noreply.github.com>
2026-08-20 15:14:46 +0530

    feat(coupon): allow coupon code at checkout

--- src/main/java/com/bookstore/dto/request/CheckoutRequest.java
+++ src/main/java/com/bookstore/dto/request/CheckoutRequest.java
@@ -15,4 +15,7 @@ public class CheckoutRequest {
     @NotBlank(message = "Shipping address is required")
     @Size(max = 500, message = "Shipping address must not exceed 500 characters")
     private String shippingAddress;
+
+    @Size(max = 40, message = "Coupon code must not exceed 40 characters")
+    private String couponCode;
 }
