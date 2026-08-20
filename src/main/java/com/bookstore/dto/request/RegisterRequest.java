4dcf96b16862a927f70da503e4cb8b5d972d878c
Pankaj Kumar <83492104+hack2ai@users.noreply.github.com>
2026-08-20 14:22:48 +0530

    security: tighten registration input constraints

--- src/main/java/com/bookstore/dto/request/RegisterRequest.java
+++ src/main/java/com/bookstore/dto/request/RegisterRequest.java
@@ -20,10 +20,11 @@ public class RegisterRequest {
 
     @NotBlank(message = "Email is required")
     @Email(message = "Invalid email format")
+    @Size(max = 150, message = "Email must not exceed 150 characters")
     private String email;
 
     @NotBlank(message = "Password is required")
-    @Size(min = 8, message = "Password must be at least 8 characters")
+    @Size(min = 8, max = 72, message = "Password must be between 8 and 72 characters")
     private String password;
 
     @Size(max = 20, message = "Phone number too long")
