f1806e79dcb894e9e456b73c583a5c467830a320
Pankaj Kumar <83492104+hack2ai@users.noreply.github.com>
2026-08-20 14:23:30 +0530

    feat(auth): add refresh token repository

---
+++ src/main/java/com/bookstore/repository/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package com.bookstore.repository;
+
+import com.bookstore.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findByTokenHashAndRevokedAtIsNull(String tokenHash);
+}
