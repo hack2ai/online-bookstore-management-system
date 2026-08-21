d61c90c03e9eedd693a9aa0321eb91b77e0c4c22
Pankaj Kumar <83492104+hack2ai@users.noreply.github.com>
2026-08-21 13:38:22 +0530

    Support ranged admin analytics

--- src/main/java/com/bookstore/service/AdminAnalyticsService.java
+++ src/main/java/com/bookstore/service/AdminAnalyticsService.java
@@ -2,6 +2,9 @@ package com.bookstore.service;
 
 import com.bookstore.dto.response.AdminAnalyticsResponse;
 
+import java.time.LocalDate;
+
 public interface AdminAnalyticsService {
     AdminAnalyticsResponse getAnalytics();
+    AdminAnalyticsResponse getAnalytics(LocalDate from, LocalDate to);
 }
