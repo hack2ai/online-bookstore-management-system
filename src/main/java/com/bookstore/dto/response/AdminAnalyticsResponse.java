05834e95a0edf73bb073e77cec9c4c108dd3e5ea
Pankaj Kumar <83492104+hack2ai@users.noreply.github.com>
2026-08-21 13:04:39 +0530

    Expand admin analytics metrics

--- src/main/java/com/bookstore/dto/response/AdminAnalyticsResponse.java
+++ src/main/java/com/bookstore/dto/response/AdminAnalyticsResponse.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Data
 @Builder
@@ -15,4 +16,8 @@ public class AdminAnalyticsResponse {
     private BigDecimal paidRevenue;
     private long paidOrders;
     private BigDecimal averageOrderValue;
+    private long pendingOrders;
+    private long cancelledOrders;
+    private long lowStockBooks;
+    private List<BestSellingBookResponse> bestSellingBooks;
 }
