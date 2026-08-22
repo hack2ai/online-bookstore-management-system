6a1a32016c49e6273e65daace2d11903a760d981
Pankaj Kumar <83492104+hack2ai@users.noreply.github.com>
2026-08-22 15:14:35 +0530

    fix(test): remove duplicate static import

--- src/test/java/com/bookstore/service/RequestMetricsServiceTest.java
+++ src/test/java/com/bookstore/service/RequestMetricsServiceTest.java
@@ -3,7 +3,6 @@ package com.bookstore.service;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RequestMetricsServiceTest {
 
