d5cb562db322fa44a7e1e36228fd83757ff2dc0a
Pankaj Kumar <83492104+hack2ai@users.noreply.github.com>
2026-09-10 08:14:52 +0530

    feat(api-docs): enrich OpenAPI project metadata

--- src/main/java/com/bookstore/config/OpenApiConfig.java
+++ src/main/java/com/bookstore/config/OpenApiConfig.java
@@ -2,6 +2,7 @@ package com.bookstore.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
@@ -20,6 +21,9 @@ public class OpenApiConfig {
                         .title("Online Bookstore Management System API")
                         .version("1.0.0")
                         .description("REST API for authentication, catalog, cart, orders, payments, reviews, wishlist, and administration.")
+                        .contact(new Contact()
+                                .name("Online Bookstore Management System")
+                                .url("https://github.com/hack2ai/online-bookstore-management-system"))
                         .license(new License().name("Project License")))
                 .components(new Components()
                         .addSecuritySchemes(BEARER_SCHEME,
